222839372095034ddc5e8259475440e8a6942796
DKodak <44468461+DKodak@users.noreply.github.com>
2019-05-22 16:21:47 +0800

    mybaits逆向生成demo

---
+++ mybaits_generators/src/main/java/com/ht/mapper/UsersMapper.java
@@ -0,0 +1,30 @@
+package com.ht.mapper;
+
+import com.ht.pojo.Users;
+import com.ht.pojo.UsersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UsersMapper {
+    int countByExample(UsersExample example);
+
+    int deleteByExample(UsersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    Users selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
